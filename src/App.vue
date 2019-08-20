8251117189e238506edf1f5ef4542146b5f8a90b
Cardinelli <nugokvachadze@gmail.com>
2019-08-20 16:08:11 +0400

    Add navigation bar upgraded to bootstrap 4

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
     <div class="container">
-        <router-view/>
+        <br>
+        <app-header/>
+        <br>
+        <div class="row">
+            <div class="col-xs-12">
+                <router-view/>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import Header from "./components/Header.vue";
 
     export default {
-
+        components: {
+            appHeader: Header,
+        }
     }
 </script>
 
 <style>
+
+    body {
+        padding: 30px;
+        color:white;
+        display: flex;
+        margin: auto;
+        width: 100vh;
+        height: 100vh;
+        background-color: #1b2735;
+    }
 
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,27 @@
 <template>
-    <h1>Header</h1>
+    <nav class="navbar navbar-light bg-light">
+        <div class="navbar-header">
+            <router-link class="navbar-brand" to="/">
+                Stock Trader
+            </router-link>
+        </div>
+        <form class="form-inline">
+            <router-link class="alink" to="/portfolio"
+                         active-class="active"><a class="btn  btn-outline-info" > Portfolio </a></router-link>
+            <router-link class="alink" to="/stocks"
+                         active-class="active"><a class="btn  btn-outline-info"> Stocks </a></router-link>
+            <router-link class="alink" to="/"
+                         active-class="active"><a class="btn  btn-outline-info"> End Day </a></router-link>
+
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Save / Load
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Save Data</a>
+                <a class="dropdown-item" href="#">Load Data</a>
+            </div>
+        </form>
+    </nav>
 </template>
 
 <script>
@@ -9,5 +31,19 @@
 </script>
 
 <style scoped>
+
+    .alink {
+        border:none;
+        margin: 2px;
+        text-decoration: none;
+        color: #1b2735;
+    }
+
+    .alink:hover {
+        transition: 0.5s;
+        background-color: deepskyblue;
+        border-radius: 50px;
+        color: white;
+    }
 
 </style>
