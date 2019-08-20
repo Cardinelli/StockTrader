04dd0450abbd230f5f385f24b68a00797a327598
Cardinelli <nugokvachadze@gmail.com>
2019-08-20 16:26:54 +0400

    Change navbar and set bootstrap version on 3

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,26 +1,27 @@
 <template>
-    <nav class="navbar navbar-light bg-light">
-        <div class="navbar-header">
-            <router-link class="navbar-brand" to="/">
-                Stock Trader
-            </router-link>
-        </div>
-        <form class="form-inline">
-            <router-link class="alink" to="/portfolio"
-                         active-class="active"><a class="btn  btn-outline-info" > Portfolio </a></router-link>
-            <router-link class="alink" to="/stocks"
-                         active-class="active"><a class="btn  btn-outline-info"> Stocks </a></router-link>
-            <router-link class="alink" to="/"
-                         active-class="active"><a class="btn  btn-outline-info"> End Day </a></router-link>
-
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Save / Load
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="#">Save Data</a>
-                <a class="dropdown-item" href="#">Load Data</a>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <router-link to="/" class="navbar-brand"> Stock Trader </router-link>
             </div>
-        </form>
+            <ul class="nav navbar-nav">
+                <router-link to="/portfolio" active-class="active" tag="li"><a> Portfolio </a></router-link>
+                <router-link to="/stocks" active-class="active" tag="li"><a> Stocks </a></router-link>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li active-class="active"><a href="#"> End Day </a></li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Save / Load
+                        <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Page 1-1</a></li>
+                        <li><a href="#">Page 1-2</a></li>
+                    </ul>
+                </li>
+            </ul>
+
+        </div>
     </nav>
 </template>
 
@@ -31,19 +32,5 @@
 </script>
 
 <style scoped>
-
-    .alink {
-        border:none;
-        margin: 2px;
-        text-decoration: none;
-        color: #1b2735;
-    }
-
-    .alink:hover {
-        transition: 0.5s;
-        background-color: deepskyblue;
-        border-radius: 50px;
-        color: white;
-    }
 
 </style>
