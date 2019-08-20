e1f45b47fdb0654c789fa96033c60aa06e3ccdae
Cardinelli <nugokvachadze@gmail.com>
2019-08-20 17:06:46 +0400

    Add Stocks page and Bootstrap styles

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,13 +1,32 @@
 <template>
-    <h1>Stock</h1>
+    <div class="col-sm-6 col-md-5">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                    Name
+                    <small> (price: PRICE) </small>
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="pull-left">
+                    <input
+                            type="number"
+                            class="form-control"
+                            placeholder="Quantity">
+                </div>
+                <div class="pull-right inline">
+                    <button class="btn btn-success">Buy</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-    export default {
-        name: "Stock"
-    }
 </script>
 
 <style scoped>
+    div {
+    }
 
 </style>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,10 +1,26 @@
 <template>
-    <h1>Stocks</h1>
+    <div>
+        <app-stock v-for="stock in stocks"> </app-stock>
+
+    </div>
 </template>
 
 <script>
+    import Stock from "./Stock.vue";
     export default {
-        name: "Stocks"
+        data() {
+            return {
+                stocks: [
+                    {id: 1, name: 'BmW', price: 100},
+                    {id: 2, name: 'Apple', price: 200},
+                    {id: 3, name: 'Opel', price: 300},
+                    {id: 4, name: 'Twitter', price: 500},
+                ]
+            }
+        },
+        components: {
+            appStock: Stock,
+        }
     }
 </script>
 
