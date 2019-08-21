3ff81f1d44eec82fc852aa32ef1db03f006152b4
Cardinelli <nugokvachadze@gmail.com>
2019-08-21 11:35:27 +0400

    Add Sell Stock function in portfolio

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -16,12 +16,12 @@ const mutations = {
         }
         state.funds -= stockPrice * quantity;
     },
-    'SELL_STOCK'(state, {stockId, quantity, stockprice}) {
+    'SELL_STOCK'(state, {stockId, quantity, stockPrice}) {
         const record = state.stocks.find(element => element.id == stockId);
         if (record.quantity > quantity) {
             record.quantity -= quantity;
         } else {
-            state.stock.splice(state.stock.indexOf(record), 1);
+            state.stocks.splice(state.stocks.indexOf(record), 1);
         }
         state.funds += stockPrice * quantity;
     }
